a2728f8e755f795b7271bd64c9617cb1586d4e21
will-felipe-souza <will.felipe.souza@gmail.com>
2025-02-09 19:35:00 -0300

    refactor: Retirando códigos do PWA

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("service-worker.js")
-    .then(() => {
-      console.log("Service Worker registrado!")
-    })
-    .catch((error) => {
-      console.error("Erro ao registrar Service Worker:", error)
-    })
-}
-
 const cardsArray = [
   "🍎",
   "🍎",
